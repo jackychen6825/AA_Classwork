83a564e579f20a8d7f751568f809124ec43948cb
RobertT122 <robert@tetrault.org>
2021-09-22 12:35:45 -0400

    Knight and King added

---
+++ W4/D3/Chess/knight_king.rb
@@ -0,0 +1,50 @@
+require_relative "piece"
+require_relative "stepable"
+
+class Knight < Piece
+  include Stepable
+
+  attr_reader :symbol
+  def initialize(color, board, pos)
+    super
+    @symbol = :N
+  end
+
+  def moves_diff
+    [
+      [-2, -1]
+      [-2, 1]
+      [2, -1]
+      [2, 1]
+      [-1, 2]
+      [1, 2]
+      [-1, -2]
+      [1, -2]
+    ]
+  end
+
+end
+
+class King < Piece
+  include Stepable
+
+  attr_reader :symbol
+  def initialize(color, board, pos)
+    super
+    @symbol = :K
+  end
+
+  def moves_diff
+    [
+      [-1, -1]
+      [-1, 0]
+      [-1, 1]
+      [0, 1]
+      [1, 1]
+      [1, 0]
+      [1, -1]
+      [0, -1]
+    ]
+  end
+
+end
