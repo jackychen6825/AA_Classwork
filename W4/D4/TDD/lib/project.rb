ab14fb91a53967632a8558c19085f11570c7838d
mattpettenato <mattpettenato@gmail.com>
2021-09-23 14:23:02 -0700

    Worked on transpose two sum

--- W4/D4/TDD/lib/project.rb
+++ W4/D4/TDD/lib/project.rb
@@ -1,11 +1,37 @@
+class Array
 
-def my_uniq(arr)
-  count = Hash.new(0)
+  def my_uniq
+    count = Hash.new(0)
 
-  arr.each do |ele|
-    count[ele] += 1
-  end 
+    self.each do |ele|
+      count[ele] += 1
+    end 
   
-  count.keys
-end 
+    count.keys
+  end 
 
+  def two_sum
+    pairs = []
+    self.each.with_index do |num1, i1|
+      self.each.with_index do |num2, i2|
+        if i2 > i1 && num1 + num2 == 0
+          pairs << [i1, i2]
+        end
+      end
+    end
+    return pairs
+  end
+
+  def my_transpose
+    transposed = []
+    (0...self.length).each do |col_idx|
+      place_holder = []
+      (0...self.length).each do |row_idx|
+        place_holder << self[row_idx][col_idx]
+      end
+      transposed << place_holder
+    end
+    transposed
+  end
+
+end
--- W4/D4/TDD/spec/project_spec.rb
+++ W4/D4/TDD/spec/project_spec.rb
@@ -3,11 +3,36 @@ require 'project'
 describe '#my_uniq' do 
   let (:first_arr) { [1, 2, 1, 3, 3] }
   it 'returns unique elements' do 
-    expect(my_uniq(first_arr)).to eq([1,2,3])
+    expect(first_arr.my_uniq).to eq([1,2,3])
   end 
 
   it "doesn't modify first arr" do 
-    my_uniq(first_arr)
+    first_arr.my_uniq
     expect(first_arr).to be(first_arr)
   end 
 end 
+
+describe '#two_sum' do
+  let (:first_arr) { [-1, 0, 2, -2, 1] }
+  it 'returns dictionary-wise index of pairs that sum to zero' do 
+    expect(first_arr.two_sum).to eq([[0, 4], [2, 3]])
+  end
+end
+
+describe '#my_transpose' do 
+  let (:rows) { [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8]
+  ] }
+  let (:cols) { [
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8]
+  ]}
+  it 'will transpose rows into cols, cols into rows.' do
+    expect(rows.my_transpose).to eq(cols)
+    expect(cols.my_transpose).to eq(rows)
+  end
+
+end
