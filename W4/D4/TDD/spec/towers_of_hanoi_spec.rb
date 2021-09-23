ab9cb75bfee6004b2ccde4df33c35b3c3c9006ac
mattpettenato <mattpettenato@gmail.com>
2021-09-23 16:57:49 -0700

    Finished Hanoi

--- W4/D4/TDD/spec/towers_of_hanoi_spec.rb
+++ W4/D4/TDD/spec/towers_of_hanoi_spec.rb
@@ -1,8 +1,9 @@
 require 'towers_of_hanoi'
 
-describe Towersgame do
+describe Towersgame do
   subject(:tower) {Towersgame.new}
-
+  let (:tower2) {Towersgame.new} 
+  
   describe '#initialize' do 
     it 'creates new towers' do
       expect(tower.towers).to eq([[4,3,2,1], [], []])
@@ -17,9 +18,17 @@ describe Towersgame do
     end
 
     it 'can never have larger ring above smaller ring' do 
-      tower.towers = [[4,3], [2], [1]]
-      expect{tower.move(0,2)}.to raise("Error: larger ring above smaller ring")
+      tower2.towers = [[4,3,2], [], [1]]
+      
+      expect{tower2.move(0,2)}.to raise_error("Error: larger ring is above smaller ring")
     end 
 
   end
-end 
+
+  describe '#winner?' do
+    it 'You win when the game is over' do
+    tower.towers = [[], [], [4, 3, 2, 1]]
+    expect(tower.winner?).to eq('you win!')
+    end
+  end
+end
