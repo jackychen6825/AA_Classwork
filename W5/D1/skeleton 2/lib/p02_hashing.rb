8ec655e27f23b2422cf0cb0998b62ea936fbddae
ntademaru <ntademaru@gmail.com>
2021-09-27 16:38:04 -0700

    Began creating array hash

--- W5/D1/skeleton 2/lib/p02_hashing.rb
+++ W5/D1/skeleton 2/lib/p02_hashing.rb
@@ -1,9 +1,18 @@
 class Integer
   # Integer#hash already implemented for you
+  self.hash
 end
 
 class Array
-  def hash
+
+  def hash #[1,2,3]
+  integers = self.map { |ele| ele.to_i }
+  hash = integers.shift
+  integers.each do |num|
+    hash = (hash ^ num).to_s(2).to_i
+  end
+
+  hash
   end
 end
 
