c07d7a4f37320c3b2bf57de56eead2dc141d87e9
jacky <jc6825@stern.nyu.edu>
2021-09-27 20:06:07 -0400

    Create hash methods

--- W5/D1/skeleton 2/lib/p02_hashing.rb
+++ W5/D1/skeleton 2/lib/p02_hashing.rb
@@ -1,30 +1,52 @@
 class Integer
   # Integer#hash already implemented for you
-  self.hash
 end
 
 class Array
 
-  def hash #[1,2,3]
-  integers = self.map { |ele| ele.to_i }
-  hash = integers.shift
-  integers.each do |num|
-    hash = (hash ^ num).to_s(2).to_i
+  def hash 
+
+    hash_values = []
+    self.each.with_index do |num, idx|
+      hash_values << (num + idx).hash
+    end 
+    return hash_values.sum 
   end
 
-  hash
-  end
 end
 
 class String
   def hash
+    hash_values = []
+    alphabet = ("a".."z").to_a
+
+    self.each_char.with_index do |char, idx|
+      char = char.downcase 
+      hash_values << (alphabet.index(char) + idx).hash
+    end 
+    hash_values.sum
   end
 end
 
-class Hash
+class Hash 
   # This returns 0 because rspec will break if it returns nil
   # Make sure to implement an actual Hash#hash method
   def hash
-    0
+  
+    hash_values = []
+    hash.each do |key, value|
+      string = letter_hash(key.to_s) + letter_hash(value)
+      hash_values << string 
+    end 
+    hash_values.sum 
+    # 0
   end
+
+  def letter_hash(char)
+      alphabet = ("a".."z").to_a
+      char = char.downcase 
+      alphabet.index(char).hash
+  end 
+
 end
+
