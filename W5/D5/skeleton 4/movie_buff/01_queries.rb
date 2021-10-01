d4bbb4e55b06cf2abbe3fbe689472715e0b62253
jacky <jc6825@stern.nyu.edu>
2021-10-01 15:16:25 -0400

    Complete first 3 queries

--- W5/D5/skeleton 4/movie_buff/01_queries.rb
+++ W5/D5/skeleton 4/movie_buff/01_queries.rb
@@ -6,6 +6,9 @@ def it_was_ok
   # We can use ranges (a..b) inside a where method.
   #
   # Find the id, title, and score of all movies with scores between 2 and 3
+    Movie
+      .where(score: 2..3)
+      .select(:id, :title, :score)
 
 end
 
@@ -20,6 +23,11 @@ def harrison_ford
   #
   # Find the id and title of all movies in which Harrison Ford
   # appeared but not as a lead actor
+
+  Movie
+    .joins(:actors) 
+    .where(["name = ? and castings.ord != ?", "Harrison Ford", 1])
+    .select(:id, :title)   
 
 end
 
@@ -37,6 +45,13 @@ def biggest_cast
   #
   # Find the id and title of the 3 movies with the
   # largest casts (i.e most actors)
+
+  Movie
+    .joins(:castings)
+    .group("movies.id")
+    .order("COUNT(castings.id) DESC")
+    .limit(3) 
+    .select(:id, :title) 
 
 end
 
