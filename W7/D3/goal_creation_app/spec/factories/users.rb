354fb3bd1461f360f72bdf8263837dcaf6e2cd55
jacky <jc6825@stern.nyu.edu>
2021-10-13 18:01:16 -0400

    Progress

--- W7/D3/goal_creation_app/spec/factories/users.rb
+++ W7/D3/goal_creation_app/spec/factories/users.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :user do
+    username { Faker::Movies::HarryPotter.character }
+    password { "password" }
     
   end
 end
--- W7/D3/goal_creation_app/spec/models/user_spec.rb
+++ W7/D3/goal_creation_app/spec/models/user_spec.rb
@@ -2,4 +2,30 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  it { should validate_presence_of(:username) }
+  it { should validate_presence_of(:password_digest) }
+  it { should validate_presence_of(:session_token) }
+
+  it { should validate_length_of(:password).is_at_least(6) }
+
+  describe "is_valid_password?" do 
+    let!(:user) { create(:user) }
+    context "with a valid password" do 
+      it "should return true" do 
+        expect(user.is_valid_password?('password')).to be true 
+      end 
+    end 
+    context "with an invalid password" do 
+      it "should return false" do 
+        expect(user.is_valid_password?('peanuts')).to be false   
+      end 
+    end 
+  end 
+  
+  describe "User::find_by_credentials" do 
+    let!(:user) { create(:user) } #random hp char w pass of password 
+    
+  end 
+
 end
