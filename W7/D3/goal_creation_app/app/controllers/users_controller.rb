9ee8803be160ba98d95ac3575e1aca18cb2a51dd
Chris <chris.cheasty3@gmail.com>
2021-10-13 14:42:52 -0700

    Create user model and controller methods

--- W7/D3/goal_creation_app/app/controllers/users_controller.rb
+++ W7/D3/goal_creation_app/app/controllers/users_controller.rb
@@ -1,7 +1,22 @@
 class UsersController < ApplicationController
   def new
+    render :new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_url(@user)
+    else
+      flash.now[:errors] = @user.errors.full_messages
+      render :new
+    end
   end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+
 end
--- W7/D3/goal_creation_app/config/routes.rb
+++ W7/D3/goal_creation_app/config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
+  # get 'users/new'
+  # get 'users/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [:new, :create]
 end
