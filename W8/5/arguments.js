7688502d03af0fa7016fa19844c7fb454b6e33f7
jacky <jc6825@stern.nyu.edu>
2021-10-22 19:11:28 -0400

    Complete

--- W8/5/arguments.js
+++ W8/5/arguments.js
@@ -112,13 +112,30 @@ function curriedSum(args) {
 Function.prototype.curry = function (numArgs) {
     let allArgs = [];
     let func = this;
-    if (allArgs.length < numArgs) {
-        return function () {
-            func 
+
+    return function _curriedArgs(arg) {
+        allArgs.push(arg); 
+        if (allArgs.length < numArgs) {
+            return _curriedArgs 
+        } else {
+            return func.apply(null, allArgs);
         }
-    } else {
-        func.apply(, allArgs)
     }
 }
 
-// ctx.method.curry?
+function sumThree(num1, num2, num3) {
+    return num1 + num2 + num3;
+}
+
+sumThree(4, 20, 6); // == 30
+
+// you'll write `Function#curry`!
+let f1 = sumThree.curry(3); // tells `f1` to wait until 3 arguments are given before running `sumThree`
+f1 = f1(4); // [Function]
+f1 = f1(20); // [Function]
+f1 = f1(6); // = 30
+
+// or more briefly:
+console.log(sumThree.curry(3)(4)(20)(6)); // == 30
+
+
