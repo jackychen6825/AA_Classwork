4b9132a3aeb44cc3da7ead660e2c6cf39ce4f7ef
Williamsliam23 <williams.liam23@gmail.com>
2021-10-22 18:27:45 -0500

    Object inheritance with new and create

---
+++ W8/5/inheritence.js
@@ -0,0 +1,34 @@
+Function.prototype.inherits = function (parent) {
+  // function Surrogates() {};
+  // Surrogates.prototype = parent.prototype;
+  // this.prototype = new Surrogates();
+  // this.prototype.constructor = parent;
+  this.prototype = Object.create(parent.prototype);
+  this.prototype.constructor = this;
+}
+
+
+
+
+
+function MovingObject () {}
+
+MovingObject.prototype.move = function () {
+console.log('Is moving!')
+}
+
+function Ship () {}
+Ship.inherits(MovingObject);
+
+Ship.prototype.sail = function () {
+  console.log('Is Sailing')
+}
+
+function Asteroid () {}
+Asteroid.inherits(MovingObject);
+
+const s = new Ship();
+const a = new Asteroid();
+
+s.move();
+a.move();
